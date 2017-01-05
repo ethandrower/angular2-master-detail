499e79d8c2970c46855c9557d6cce5a198078baa
ethan drower <ethan drower>
2017-01-05 12:00:40 -0500

    init commit of heroes tutorial

---
+++ app/dashboard.component.ts
@@ -0,0 +1,28 @@
+
+
+import { Component } from '@angular/core';
+
+import {HeroService } from './hero.service';
+
+import {Hero } from './hero';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-dashboard',
+  templateUrl: 'dashboard.component.html'
+})
+
+export class DashboardComponent {
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+    .then(heroes => this.heroes = heroes.slice(1,5));
+
+  }
+
+}
+
